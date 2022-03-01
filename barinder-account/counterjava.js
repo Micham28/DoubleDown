fabc2dae121cfe62bacfb3aba6c6d6063babe3e2
Micham28 <100454576+Micham28@users.noreply.github.com>
2022-03-01 15:10:06 -0800

    update something

--- barinder-account/counterjava.js
+++ barinder-account/counterjava.js
@@ -1,6 +1,16 @@
+"counter"
 var clicks = 0;
 
 function onClick() {
   clicks += 1;
   document.getElementById("clicks").innerHTML = clicks;
 };
+
+"fold dunction"
+
+const items = document.querySelectorAll(".QA a");
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+items.forEach(item => item.addEventListener('click', toggleAccordion));
